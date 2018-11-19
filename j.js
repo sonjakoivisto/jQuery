1c2fa262eb1ce148ea9411c3ea7a0e8ea01cffbd
sonjakoivisto <37457729+sonjakoivisto@users.noreply.github.com>
2018-11-19 18:38:21 +0200

    Add files via upload

--- j.js
+++ j.js
@@ -1,2 +1,22 @@
+$("input").keypress(function(event){
+    if(event.which === 13){
+        alert("You hit enter");
+    }
+    console.log(event);
+});
 
+$("button").click(function(){
+    $(this).css("background", "pink");
+});
 
+$("h1").on("click", function(){
+    $(this).css("color", "purple");
+});
+
+$("button").on("mouseenter", function(){
+    $(this).css("font-weight", "bold");
+});
+
+$("button").on("mouseleave", function(){
+    $(this).css("font-weight", "normal");
+});
